b984098dc8fae0c0c1d639dabc0ed6f16ff92802
fedi abdouli <67550421+cs-fedy@users.noreply.github.com>
2022-10-05 15:37:04 +0100

    signIn ui fixed

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import {
   Button,
   DividerWithText,
+  EmailInput,
   icons,
   layouts,
+  PasswordInput,
   SocialMediaButton,
 } from "../components";
 import { useApp } from "../contexts/AppContext";
@@ -17,80 +19,62 @@ function SignIn() {
   };
 
   return (
-    <div className="flex h-screen justify-center py-6">
-      <div className="flex w-full flex-col items-center justify-between">
-        <layouts.BackBarLayout />
-        <h1 className="mx-6 text-5xl font-bold">Login to your Account</h1>
-        <div className="flex w-full flex-col items-center gap-5 px-6">
-          <layouts.InputLayout>
-            <icons.Email color="fill-gray-500" />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="flex-1 border-0 bg-transparent text-sm font-regular leading-normal tracking-wider outline-none"
-            />
-          </layouts.InputLayout>
-          <layouts.InputLayout>
-            <icons.Lock color="fill-gray-500" />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="flex-1 bg-transparent text-sm font-regular leading-normal tracking-wider outline-none"
-            />
-            <icons.Hide color="fill-gray-500" />
-          </layouts.InputLayout>
-          <div className="flex items-center">
-            {/* TODO: customize this later */}
-            <input id="remember-me" type="checkbox" className="h-6 w-6" />
-            <label
-              htmlFor="remember-me"
-              className="ml-4 text-sm font-semi-bold leading-normal tracking-wider text-gray-900"
-            >
-              Remember me
-            </label>
-          </div>
-          <Button
-            // the button should be disabled if the user didn't provide
-            // valid input. I disable it for testing purposes.
-            // isDisabled
-            handleNextStep={logUser}
+    <div className="flex h-screen w-full flex-col items-center justify-between px-6 pt-6 pb-12">
+      <layouts.BackBarLayout />
+      <h1 className="text-5xl font-bold">Login to your Account</h1>
+      <div className="flex w-full flex-col items-center gap-8">
+        <EmailInput />
+        <PasswordInput />
+        <div className="flex items-center">
+          {/* TODO: customize this later */}
+          <input id="remember-me" type="checkbox" className="h-6 w-6" />
+          <label
+            htmlFor="remember-me"
+            className="ml-4 text-sm font-semi-bold leading-normal tracking-wider text-gray-900"
           >
-            Sign in
-          </Button>
-          <Link
-            to="/resetPassword"
-            className="text-sm font-regular leading-normal tracking-wider text-primary-500"
-          >
-            Forget the password?
-          </Link>
+            Remember me
+          </label>
         </div>
-        <div className="flex w-full flex-col items-center gap-5 px-6">
-          <DividerWithText>or continue with</DividerWithText>
-          <div className="flex w-full items-center gap-3 px-6">
-            <SocialMediaButton submitHandler={logUser}>
-              <icons.Facebook />
-            </SocialMediaButton>
-            <SocialMediaButton submitHandler={logUser}>
-              <icons.Google />
-            </SocialMediaButton>
-            <SocialMediaButton submitHandler={logUser}>
-              <icons.Apple />
-            </SocialMediaButton>
-          </div>
+        <Button
+          // the button should be disabled if the user didn't provide
+          // valid input. I disable it for testing purposes.
+          // isDisabled
+          // TODO: work on the disabled feature
+          handleNextStep={logUser}
+        >
+          Sign in
+        </Button>
+        <Link
+          to="/resetPassword"
+          className="text-sm font-regular leading-normal tracking-wider text-primary-500"
+        >
+          Forget the password?
+        </Link>
+      </div>
+      <div className="flex w-full flex-col items-center gap-5">
+        <DividerWithText>or continue with</DividerWithText>
+        <div className="flex w-full items-center gap-5 px-6">
+          <SocialMediaButton submitHandler={logUser}>
+            <icons.Facebook />
+          </SocialMediaButton>
+          <SocialMediaButton submitHandler={logUser}>
+            <icons.Google />
+          </SocialMediaButton>
+          <SocialMediaButton submitHandler={logUser}>
+            <icons.Apple />
+          </SocialMediaButton>
         </div>
-        <div className="flex gap-2">
-          <span className="text-sm font-regular leading-normal tracking-wider text-gray-500">
-            Don&apos;t have an account?
-          </span>
-          <Link
-            to="/signUp"
-            className="text-sm font-regular leading-normal tracking-wider text-primary-500"
-          >
-            Sign up
-          </Link>
-        </div>
+      </div>
+      <div className="flex gap-2">
+        <span className="text-sm font-regular leading-normal tracking-wider text-gray-500">
+          Don&apos;t have an account?
+        </span>
+        <Link
+          to="/signUp"
+          className="text-sm font-regular leading-normal tracking-wider text-primary-500"
+        >
+          Sign up
+        </Link>
       </div>
     </div>
   );
