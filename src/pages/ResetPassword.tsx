eddf4411a41e624c6a1c362c564493bfd4b9806f
fedi abdouli <67550421+cs-fedy@users.noreply.github.com>
2022-10-05 17:11:38 +0100

    reset password logic refactored

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Stepper, screens } from "../components";
+import { screens, SetupModal, layouts } from "../components";
+import { useSteps } from "../hooks";
+
+const steps = [
+  {
+    step: <screens.ChooseResetMethodStep />,
+    title: "Forgot Password",
+    canSkip: false,
+  },
+  {
+    step: <screens.ConfirmationCodeStep />,
+    title: "Forgot Password",
+    canSkip: false,
+  },
+  {
+    step: <screens.CreateNewPasswordStep />,
+    title: "Create New Password",
+    canSkip: false,
+  },
+];
 
 function ResetPassword() {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
-  const handleFinalStep = () => {
-    setTimeout(() => navigate("/signIn"), 2000);
-  };
+  const { currentStep, handleNextStep, skipStep } = useSteps({
+    steps,
+    handleFinalStep() {
+      setIsOpen(true);
+      setTimeout(() => navigate("/signIn"), 2000);
+    },
+  });
 
-  const steps = [
-    screens.ChooseResetMethodStep,
-    screens.ConfirmationCodeStep,
-    screens.CreateNewPasswordStep,
-  ];
-
-  return <Stepper handleFinalStep={handleFinalStep} stepsComponents={steps} />;
+  return (
+    <>
+      <SetupModal isModalOpen={isOpen} />
+      <layouts.StepLayout
+        title={currentStep.title}
+        nextStepHandler={handleNextStep}
+        canSkip={currentStep.canSkip}
+        skipHandler={skipStep}
+      >
+        {currentStep.step}
+      </layouts.StepLayout>
+    </>
+  );
 }
 
 export default ResetPassword;
